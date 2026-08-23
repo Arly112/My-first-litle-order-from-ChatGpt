3721a876cd71ce6b2a3a7a7b35553ee2648ded61
Arly <pechnikov.danya08@gmail.com>
2026-08-23 15:44:44 +0300

    Добавьте файлы проекта.

---
+++ My first litle order from ChatGpt/Draft.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+struct Product
+{
+	int ID, Price, Count;
+	std::string Name;
+};
+
+void cout_Menu()
+{
+	std::cout << "1.Показать все товары" << std::endl;
+	std::cout << "2.Добавить товар" << std::endl;
+	std::cout << "3.Удалить товар" << std::endl;
+	std::cout << "4.Найти товар" << std::endl;
+	std::cout << "5.Продать товар" << std::endl;
+	std::cout << "6.Пополнить товар" << std::endl;
+	std::cout << "7.Показать товары, которых мало" << std::endl;
+	std::cout << "8.Сохранить данные" << std::endl;
+	std::cout << "9.Загрузить данные" << std::endl;
+	std::cout << "0.Выход" << std::endl;
+}
